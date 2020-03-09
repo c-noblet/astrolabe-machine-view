1a0295db1d805880df6b325371e395ee26a89852
c-noblet <c.noblet.dev@gmail.com>
2020-03-09 10:57:18 +0100

    bootstrapVue + Sass+ vue.config.js setup

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
+import './scss/main.scss'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
