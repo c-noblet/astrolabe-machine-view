654a870dfaf05584d950eeff8bff11f5047102e6
c-noblet <c.noblet.dev@gmail.com>
2020-03-09 15:20:26 +0100

    Window position and size handler added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,41 @@
 <template>
   <div>
-  <Window :url="'https://www.lastrolabe.org/'"/>
+    <ul>
+      <li v-for="(item, index) in windows" :key="index">
+        <Window 
+          :url="item.url"
+          :width="item.width"
+          :height="item.height"
+          :posX="item.posX"
+          :posY="item.posY"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 <script>
-  import Window from './Window.vue'
+import Window from './Window.vue'
 export default {
   name: 'Home',
-  props: {
-    msg: String
+  data () {
+    return {
+      windows: [
+        { 
+          url: 'http://www.lastrolabe.org/',
+          width: 50,
+          height: 50,
+          posX: 0,
+          posY: 0 
+        },
+        { 
+          url: 'http://www.lastrolabe.org/',
+          width: 50,
+          height: 50,
+          posX: 50,
+          posY: 50  
+        }
+      ]
+    }
   },
   components: {
     Window,
--- src/components/Window.vue
+++ src/components/Window.vue
@@ -1,18 +1,39 @@
 <template>
-  <div>
-  <iframe :src = "url"> </iframe>
+  <div class="window" :style="'width:'+width+'%;height:'+height+'%;left:'+posY+'%;top:'+posY+'%'">
+    <div>
+      <b-button variant="warning">Modifier</b-button>
+      <iframe :src="url"></iframe>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
-    name: 'Window',
+  name: 'Window',
   props: {
     url: String,
     posY : Number,
     posX : Number,
-    Long : Number,
-    Lag : Number
+    width : Number,
+    height : Number
   }
 }
 </script>
+<style lang="scss" scoped>
+  .window{
+    position: absolute;
+    & iframe {
+      width: 100%;
+      height: 100%;
+    }
+    & div{
+      position: relative;
+      width: 100%;
+      height: 100%;
+    }
+    & button {
+      position: absolute;
+      top: 0;
+      right: 0;
+    }
+  }
+</style>
