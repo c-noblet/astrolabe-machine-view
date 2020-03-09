03a68e77434c45bda88217e92f76714647ac4f35
ravier justin <justin.ravier@hotmail.com>
2020-03-09 14:58:22 +0100

    creation object Window

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <p>{{ msg }}</p>
+  <Window :url="'https://www.lastrolabe.org/'"/>
   </div>
 </template>
 <script>
+  import Window from './Window.vue'
 export default {
   name: 'Home',
   props: {
     msg: String
+  },
+  components: {
+    Window,
   }
 }
 </script>
---
+++ src/components/Window.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  <iframe :src = "url"> </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Window',
+  props: {
+    url: String,
+    posY : Number,
+    posX : Number,
+    Long : Number,
+    Lag : Number
+  }
+}
+</script>
