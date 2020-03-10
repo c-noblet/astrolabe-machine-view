7c34cc18a85cfb71822c1eb178458da56fe556cf
c-noblet <c.noblet.dev@gmail.com>
2020-03-10 08:51:33 +0100

    font awesome added

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import './scss/main.scss'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(faPlus)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
