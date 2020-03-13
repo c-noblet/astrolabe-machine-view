3eabf4711a101b2c6df940080fa0e26b5964d42e
ravier justin <justin.ravier@hotmail.com>
2020-03-13 11:22:41 +0100

    Gestion conflit init

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -83,7 +83,7 @@ export default {
         { 
           index: 0,
           url: 'http://www.lastrolabe.org/',
-          width: 50,
+          width: 25,
           height: 50,
           posX: 0,
           posY: 0 
@@ -91,17 +91,33 @@ export default {
         { 
           index: 1,
           url: 'http://www.lastrolabe.org/',
-          width: 50,
-          height: 50,
-          posX: 50,
-          posY: 25  
+          width: 25,
+          height: 40,
+          posX: 25,
+          posY: 60
         },
         { 
           index: 2,
           url: 'http://www.lastrolabe.org/',
-          width: 50,
+          width: 25,
           height: 50,
           posX: 0,
+          posY: 50  
+        },
+        { 
+          index: 3,
+          url: 'http://www.lastrolabe.org/',
+          width: 25,
+          height: 50,
+          posX: 50,
+          posY: 0  
+        },
+        { 
+          index: 4,
+          url: 'http://www.lastrolabe.org/',
+          width: 25,
+          height: 50,
+          posX: 50,
           posY: 50  
         }
       ]
@@ -112,6 +128,11 @@ export default {
       this.modal = window
     },
     onSubmit: function (index){
+
+      let enregistrementPosX = this.windows[index].posX
+      let enregistrementWidth = this.windows[index].width
+      // let enregistrementHeight = this.windows[index].height
+
       this.windows[index].url = this.modal.url
       this.windows[index].width = parseInt(this.modal.width)
       this.windows[index].height = parseInt(this.modal.height)
@@ -119,46 +140,120 @@ export default {
       this.windows[index].posY = parseInt(this.modal.posY)
       this.$refs['edit-modal'].hide()
 
-      this.calculPosAutreWindows(this.windows[index])
+      this.calculPosAutreWindows(this.windows[index], enregistrementPosX, enregistrementWidth/*, enregistrementHeight*/)
+      
 
     },
-    calculPosAutreWindows: function(laWindow) {
+    calculPosAutreWindows: function(laWindow, posXBase, widthBase/*, heightBase*/) {
+
+      // V1 pour conflit des iframes plus taille dynamique 
+
       for (let index in this.windows ){
         
         if ( this.windows[index] !== laWindow) {
 
+          // variables utiles
           let laWindowTop = laWindow.posY
           let laWindowBottom = laWindow.posY + laWindow.height
           let itemTop = this.windows[index].posY
           let itemBottom = this.windows[index].posY + this.windows[index].height
           let orientationH = null 
+          let choixOrientationH = null
+          let choixWidthTailleMax = null
+          let annulationModif = false
+          let widthMaxSiConflit = 80
 
           // Définie le côté de la window non modifié à adapter
-          if(laWindow.posX < this.windows[index].posX){
+          if(laWindow.posX > this.windows[index].posX){
             orientationH = 'left'
           }else{
             orientationH = 'right'
           }
 
+          // En fonction du coté de la fenetre non modifié on applique un if différent
+          if(orientationH === 'left'){
+            // fenetre non modifié à GAUCHE de laWindow
+            choixOrientationH = (this.windows[index].posX + this.windows[index].width) >= laWindow.posX
+            choixWidthTailleMax = laWindow.width <= widthMaxSiConflit
+          }else{
+            // fenetre non modifié à DROITE de laWindow
+            choixOrientationH = (laWindow.posX + laWindow.width) >= this.windows[index].posX
+            choixWidthTailleMax = (laWindow.posX + laWindow.width ) <= widthMaxSiConflit
+          }
+
+          if (laWindow.width > 100 ){
+            annulationModif = true
+          }
           
-          // Si la fenêtre modifié passe par sa droite sur une fenêtre non modifié
-          if((this.windows[index].posX + this.windows[index].width) >= laWindow.posX){
+          // Si laWindow passe sur une fenêtre non modifié celon le coté "choixOrientationH"
+          if(choixOrientationH){
+
+            // if((this.windows[index].posX + this.windows[index].width) >= laWindow.posX){
+            // if((laWindow.posX + laWindow.width) >= this.windows[index].posX){
+
             // Si les traits horizontaux des fenêtres sont en conflits sur la même ligne
             if(itemTop < laWindowBottom && itemBottom > laWindowTop){
-              // Alors on modifie la taille la fenêtre non modifié
-              this.windows[index].width = 100 - laWindow.width
-              // Si la fenêtre non modifié est à gauche
-              if(orientationH === 'left'){
-                // On redéfinie sa position
-                this.windows[index].posX = laWindow.width + laWindow.posX
+
+              // En plus du conflit, on ajoute un controle sur la largeur max de la fenetre pour éviter d'écraser la fenetre non modifier 
+              if (choixWidthTailleMax) {
+
+                // Si les deux windows prennent toute la largeur on modifie la taille la fenêtre non modifié 
+                if (((laWindow.posX + laWindow.width) + this.windows[index].width) > 100 ){
+                  if (confirm("Attention, \n\nVotre action va diminuer la largeur d'une autre fenêtre ! \n\nContinuer ?")){
+                    // Si la fenêtre non modifié est à droite 
+                    if(orientationH === 'right'){
+                    this.windows[index].width = 100 - ( laWindow.width + laWindow.posX )
+                      // On redéfinie sa position
+                      this.windows[index].posX = laWindow.width + laWindow.posX
+                    }else{
+                      // On redéfinie sa position afin de s'aligner avec la fenêtre modifié
+                      laWindow.posX = 100 - laWindow.width
+                      // On change la largeur de la fenêtre non modifié 
+                      this.windows[index].width = laWindow.posX - this.windows[index].posX
+                    }
+                  } else {
+                    annulationModif = true
+                  }
+                // Sinon on garde la taille de la windows non modifier 
+                }else{
+                  // Si la fenêtre non modifié est à gauche
+                  if(orientationH === 'right'){
+                    // On redéfinie sa position
+                    this.windows[index].posX = laWindow.width + laWindow.posX
+                  }else{
+                    // On redéfinie sa position sa position afin de s'aligner avec la fenêtre modifié
+                    laWindow.posX = 100 - laWindow.width
+                  }
+                  // on boucle sur la fonction pour faire bouger les autres Windows si besoin 
+                  // this.calculPosAutreWindows(this.windows[index], this.windows[index].posX, this.windows[index].width/*, enregistrementHeight*/)
+                  // this.onSubmit(this.windows[index])
+                }
               }else{
-                // On redéfinie sa position sa position afin de s'aligner avec la fenêtre modifié
-                laWindow.posX = 100 - laWindow.width
+                annulationModif = true
               }
+            }
+            // Si la posX + la largeur de laW est > 100
+            else if ( (laWindow.posX + laWindow.width ) > 100 ) {
+              laWindow.posX = 100 - laWindow.width
+
+              // this.calculPosAutreWindows(laWindow, laWindow.width/*, enregistrementHeight*/)
+              // this.onSubmit(this.windows[index])
+            }
+          }
+
+          if (annulationModif){
+            alert("Erreur \n\nModification(s) annulée(s)")
+              laWindow.width = widthBase
+            if(orientationH === 'right'){
+              laWindow.posX = posXBase
+            }else{
+              laWindow.posX = 100 - laWindow.width
             }
           }
         } 
       }
+
+      // v1 fin 
     }
   },
   components: {
