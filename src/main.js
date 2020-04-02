792264aab715021c2acbea3980b6a079ce86fb08
c-noblet <c.noblet.dev@gmail.com>
2020-04-02 17:55:57 +0200

    cron job init

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPlus, faEllipsisH, faPaintRoller, faSyncAlt, faUser } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { CronJob } from 'cron';
+
+const job = new CronJob('* * * * * *', function() {
+  console.log('You will see this message every second');
+}, null, true, 'America/Los_Angeles');
+job.start();
 
 library.add(faPlus, faEllipsisH, faPaintRoller, faSyncAlt, faUser)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
